72365f76b8affe24ed43a4a0339427a9b747d3bc
LASHETTY <pranaylashetty@gmail.com>
2025-01-16 17:27:01 +0530

    Update CORS configuration and deployment settings

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:3001',
+    'https://client-1ifx84bps-lashettys-projects.vercel.app',
     'https://client-5ca8pyrmd-lashettys-projects.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
